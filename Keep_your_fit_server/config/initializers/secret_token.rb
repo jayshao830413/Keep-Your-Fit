f7b2187ba5ec2b4d7925a5bf51d0f7f8397eccd8
Jay Shao <shaoshijie830413@foxmail.com>
2012-02-20 17:32:05 +0800

    server
    Signed-off-by: Jay Shao <shaoshijie830413@foxmail.com>

---
+++ Keep_your_fit_server/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KeepYourFitServer::Application.config.secret_token = '120e72364f82d29e03bc2957697f92695c47b29c347a9eef313210c5007640e233935ef67373758f3da54b87da7497b1003b80bed3270c5e6b767ac0bd08690a'
